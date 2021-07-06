db14d6093e2680dc6c70b187a7c5505940a9e8b6
novitskynick <novitskynick@gmail.com>
2021-07-06 10:32:57 +0300

    added homework1(java2)

--- src/homework1/Cat.java
+++ src/homework1/Cat.java
@@ -3,7 +3,7 @@ package homework1;
 public class Cat implements Entity {
     private int maxRunDistance;
     private int maxJumpHeight;
-    private boolean isAlive = true;
+    private boolean inGame = true;
 
     public Cat(int maxRunDistance, int maxJumpHeight) {
         this.maxRunDistance = maxRunDistance;
@@ -12,23 +12,23 @@ public class Cat implements Entity {
 
     @Override
     public void run(int size) {
-        if (!isAlive) return;
+        if (!inGame) return;
         if (size <= maxRunDistance) {
             System.out.printf("Кот пробежал %s метров по беговой дорожке.%n", size);
         } else {
             System.out.printf("Кот пытался пробежать %s метров по беговой дорожке, но не смог.%n", size);
-            isAlive = false;
+            inGame = false;
         }
     }
 
     @Override
     public void jump(int height) {
-        if (!isAlive) return;
+        if (!inGame) return;
         if (height <= maxJumpHeight) {
             System.out.printf("Кот перепрыгнул стену высотой %s метров.%n", height);
         } else {
             System.out.printf("Кот пытался перепрыгнуть стену высотой %s метров, но не смог.%n", height);
-            isAlive = false;
+            inGame = false;
         }
     }
 }
--- src/homework1/Human.java
+++ src/homework1/Human.java
@@ -4,7 +4,7 @@ public class Human implements Entity {
 
     private int maxRunDistance;
     private int maxJumpHeight;
-    private boolean isAlive = true;
+    private boolean inGame = true;
 
     public Human(int maxRunDistance, int maxJumpHeight) {
         this.maxRunDistance = maxRunDistance;
@@ -13,23 +13,23 @@ public class Human implements Entity {
 
     @Override
     public void run(int size) {
-        if (!isAlive) return;
+        if (!inGame) return;
         if (size <= maxRunDistance) {
             System.out.printf("Человек пробежал %s метров по беговой дорожке.%n", size);
         } else {
             System.out.printf("Человек пытался пробежать %s метров по беговой дорожке, но не смог.%n", size);
-            isAlive = false;
+            inGame = false;
         }
     }
 
     @Override
     public void jump(int height) {
-        if (!isAlive) return;
+        if (!inGame) return;
         if (height <= maxJumpHeight) {
             System.out.printf("Человек перепрыгнул стену высотой %s метров.%n", height);
         } else {
             System.out.printf("Человек пытался перепрыгнуть стену высотой %s метров, но не смог.%n", height);
-            isAlive = false;
+            inGame = false;
         }
     }
 }
