fe17a6fbde9b274b257b7c892be363f5864c379d
novitskynick <novitskynick@gmail.com>
2021-07-31 19:56:21 +0300

    add homework6 (java2)

---
+++ src/Server/TestServer.java
@@ -0,0 +1,11 @@
+package Server;
+
+public class TestServer {
+    public static void main(String[] args) {
+        try {
+            new Server();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
